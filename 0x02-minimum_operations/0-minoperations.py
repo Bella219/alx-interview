f882661cf87264db353fbd7cd40957809a695b8c
Bella219 <cho219bella@yahoo.com>
2023-07-13 22:43:06 +0000

    A method that calculates the fewest number of operations needed to result in exactly n H characters in the file

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,47 +1,32 @@
 #!/usr/bin/python3
-'''module contains solution to min-operations problem'''
-import math
-
-
-def highestPrimeFactor(n):
-    '''finds highest prime factor of n'''
-    max_prime = 0
-
-    while n % 2 == 0:
-        max_prime = 2
-        n = n / 2
-
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
-        while n % i == 0:
-            max_prime = i
-            n = n / i
-
-    if n > 2:
-        max_prime = n
-
-    return int(max_prime)
-
-
-def isPrime(n):
-    ''' checks whether number is prime or not '''
-    flag = False
-
-    if n > 1:
-        for i in range(2, n):
-            if (n % i) == 0:
-                flag = True
-                break
-    return not flag
+"""
+Minumum operation Model
+"""
 
 
 def minOperations(n):
-    ''' finds number of minimum of operations to get n number of H '''
-    if n <= 1:
+    """
+    In a text file, there is a single character H. Your text editor
+    can execute only two operations in this file: Copy All
+    and Paste. Given a number n, write a method that calculates
+    the fewest number of operations needed to result
+    in exactly n H characters in the file.
+    Returns an integer
+    If n is impossible to achieve, return 0
+    Example:
+    n = 9
+    H => Copy All => Paste => HH => Paste =>HHH =>
+    Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
+    Number of operations: 6
+    """
+    if type(n) is not int or n < 2:
         return 0
-
-    if n == 2 or isPrime(n):
-        return n
-
-    hpf = highestPrimeFactor(n)
-    resultOfDivision = n / hpf
-    return int(hpf + resultOfDivision)
+    result = 0
+    iteration = 2
+    while n > 1:
+        if n % iteration == 0:
+            result += iteration
+            n /= iteration
+        else:
+            iteration += 1
+    return result
